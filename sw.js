70e1e7533a816e8eb49d7cc49c1650b54cca2acf
roman1616 <32616350+roman1616@users.noreply.github.com>
2026-01-18 01:37:01 +0100

    Update asset URLs in service worker

--- sw.js
+++ sw.js
@@ -1,13 +1,10 @@
 
 const CACHE_NAME = 'cancionero-v1';
 
-  'https://www.dropbox.com/scl/fi/r3thafjal4m6zvwllz9vl/Cancionero-Pro-v1.0.html?rlkey=qvfi10gk0bnymtrzyya06s92f&st=9n2j0ink&raw=1', 
-  'https://www.dropbox.com/scl/fi/el9tkusu6sszh0epml6fh/manifest.json?rlkey=cm7gwf0x3g8b3ryserxcw0deu&st=peya6pog&raw=1'
-
 // Pega aquí los enlaces raw=1 de tus archivos
 const ASSETS = [
-  'https://www.dropbox.com',
-  'https://www.dropbox.com'
+  'https://raw.githubusercontent.com/roman1616/Cancionero-Pro/refs/heads/main/index.htm',
+  'https://raw.githubusercontent.com/roman1616/Cancionero-Pro/refs/heads/main/manifest.json'
 ];
 // Instalar y cachear archivos
 self.addEventListener('install', (e) => {
@@ -18,3 +15,4 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
+
