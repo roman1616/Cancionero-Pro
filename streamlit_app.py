b09d1bf423814666baf041e102078e7661cd4c5b
roman1616 <32616350+roman1616@users.noreply.github.com>
2026-01-30 19:53:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,15 +122,12 @@ if archivo:
         texto_js = texto_final.replace("`", "\\`").replace("$", "\\$")
         components.html(f"""
             <button id="btn"
-        style="width: 100% !important;
         background-color: #FF4B4B !important;
         color: white !important;
         border-radius: 8px !important;
         border: none !important;
-        padding: 10px 20px !important;
         font-weight: bold !important;
         height: 45px !important;
-        font-size: 14px !important;
         transition: 0.3s;">💾 GUARDAR Y COMPARTIR</button>
             <script>
                 document.getElementById('btn').onclick = async () => {{
