3cee7616189929db69b8b742619c3dc000f020b6
roman1616 <32616350+roman1616@users.noreply.github.com>
2026-01-30 19:49:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,16 @@ if archivo:
         texto_js = texto_final.replace("`", "\\`").replace("$", "\\$")
         components.html(f"""
             <button id="btn"
-        style="padding: 10px 20px !important; background: #FF4B4B; color: white; border: none; border-radius: 12px; font-weight: bold; cursor: pointer; font-size: 16px;">💾 GUARDAR Y COMPARTIR</button>
+        style="width: 100% !important;
+        background-color: #FF4B4B !important;
+        color: white !important;
+        border-radius: 8px !important;
+        border: none !important;
+        padding: 10px 20px !important;
+        font-weight: bold !important;
+        height: 45px !important;
+        font-size: 14px !important;
+        transition: 0.3s;">💾 GUARDAR Y COMPARTIR</button>
             <script>
                 document.getElementById('btn').onclick = async () => {{
                     const blob = new Blob([`{texto_js}`], {{type:'text/plain'}});
