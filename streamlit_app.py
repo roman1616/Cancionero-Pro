96a5e2fd7d38d1443005a9dc0e8bee1a35b586f7
roman1616 <32616350+roman1616@users.noreply.github.com>
2026-03-26 23:36:52 +0100

    Update default values for search and replace inputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -193,9 +193,9 @@ if st.session_state.texto_reemplazado:
 
     colb, colr = st.columns(2)
     with colb:
-        buscar = st.text_input("Buscar (Regex)", value="F'# C'#")
+        buscar = st.text_input("Buscar (Regex)", value="C'#")
     with colr:
-        reemplazar = st.text_input("Reemplazar por", value="F#' C#'")
+        reemplazar = st.text_input("Reemplazar por", value="C#'")
 
     if st.button("Aplicar reemplazo", key="btn_replace"):
         if buscar:
